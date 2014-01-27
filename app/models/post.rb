75d60d0727b8fbb21d928ada5a180240878c06bf
JC103 <calotes@ualberta.ca>
2014-01-26 18:06:37 -0700

    validations complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ActiveRecord::Base
+before_save { self.email = email.downcase }
+validates :phone, presence: true
+validates :date, presence: true
+validates :time, presence: true
+validates :cartype, presence: true
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+#uniqueness validation not necessary; allows for multiple re-submissions
 end
