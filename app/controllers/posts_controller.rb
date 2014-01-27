3d59dfe1e3eddda1cb97c394baa4154addf36d1a
JC103 <calotes@ualberta.ca>
2014-01-26 18:17:52 -0700

    form and controller basics are up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,39 @@
 class PostsController < ApplicationController
-  def new
+  def index
+  	@posts = Post.all
+  	#future search implementation:
+    #@search = Post.search(params[:q])
+    #@posts = @search.result
   end
+
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+    	flash[:success] = "Post successful!"
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    Post.find(params[:id]).destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+      params.require(:post).permit(:email, :phone, :date, :time, :cartype)
+  end
+
 end
